df9fcfd3402d3872add164d9a3c927f2f67a734b
RobertKielty <rob.kielty@gmail.com>
2020-11-15 11:18:47 +0000

    restructure the ParsTest unit test
    test now checks the reportedTest count
    and checks that each test returned is correct

--- pkg/reportedflake/ReportedFlake_test.go
+++ pkg/reportedflake/ReportedFlake_test.go
@@ -1,26 +1,61 @@
 package reportedflake
 
-import "testing"
-
-const (
-	FR_TEST = ` Which test(s) are flaking:
-[sig-instrumentation] MetricsGrabber should grab all metrics from a Scheduler
-
-Testgrid link:
-`
+import (
+	"strings"
+	"testing"
 )
 
 // Tests ParseTest which hunts for CI Job tests named in a Github Issue under
 // the heading Which tests are flaking:
-// As this is a test about tests we refer to the test cases covered here as
-// scenarios
 func TestParseTest(t *testing.T) {
-	var expectedTestCount = 1
-	tests, _ := ParseTests(FR_TEST)
-	for i, test := range tests {
-		t.Logf("Test[%d] is %v", i, test)
+	var tests = []struct {
+		name string
+		input string
+		testsWanted []string
+	}{
+		{
+			"Single test - correctly reported",
+			` Which test(s) are flaking:
+[sig-instrumentation] MetricsGrabber should grab all metrics from a Scheduler
+
+Testgrid link:
+`,
+			// Note: Array iof strings that captures order in which tests are parsed
+			[]string{ "[sig-instrumentation] MetricsGrabber should grab all metrics from a Scheduler" }, 
+		},
+		{
+			"Mulitple tests - correctly reported",
+			` Which test(s) are flaking:
+[sig-instrumentation] MetricsGrabber should grab all metrics from a Scheduler
+[sig-network] Services should be able to preserve UDP traffic when server pod cycles for a NodePort service
+Testgrid link:
+`,
+			// Note: Array iof strings that captures order in which tests are parsed
+			[]string{
+				"[sig-instrumentation] MetricsGrabber should grab all metrics from a Scheduler",
+				"[sig-network] Services should be able to preserve UDP traffic when server pod cycles for a NodePort service",
+			},
+		},
+
 	}
-	if len(tests) != expectedTestCount {
-		t.Errorf("Expected to find %d test(s) but found %d %v \n", expectedTestCount, len(tests), tests)
+
+	for _, test := range tests {
+		t.Logf("For %v", test.name)
+
+		reportedTests, _ := ParseTests(test.input)
+
+		actualTestCount := len(reportedTests)
+		expectedTestCount := len (test.testsWanted)
+
+		if actualTestCount != expectedTestCount {
+			t.Errorf("\texpected to find %d reported test(s) but found %d\n",  expectedTestCount, actualTestCount )
+			t.Errorf("\treportTests are %v\n", reportedTests )
+		}
+
+		for i, reportedTest := range reportedTests {
+			if strings.Compare(reportedTest, test.testsWanted[i]) != 0 {
+				t.Errorf("\nexpected reported test was\n%s\nbut found\n%s\n", test.testsWanted[i], reportedTest)
+			}
+		}
 	}
 }
